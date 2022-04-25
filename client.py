d9d8c2e7647f7529118cd2e6c49767c2e1d7dd8c
daniktertri <tertridanik@gmail.com>
2022-04-25 10:48:00 -0400

    add try and except

--- client.py
+++ client.py
@@ -1,17 +1,19 @@
 import socket
 import platform
+from urllib3 import Retry
+#
+#
 
-
-
+#
 HOST = "192.168.100.4"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 systemeinfo = platform.machine(),platform.version(),platform.platform(),platform.uname(),platform.processor()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    info = str(systemeinfo).encode()        
-    s.send(info)
-
-    while True:
+    try:
+      s.connect((HOST, PORT))
+      info = str(systemeinfo).encode()        
+      s.send(info)
+      while True:
 
         msg = s.recv(1024)
         msg = msg.decode()
@@ -21,4 +23,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print("this is a msg")
         else:
             print(f"Received {msg!r}")
-
+    except:
+        Retry()
+    
